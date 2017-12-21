dfa66bec8f0f58ba9e36d3718df3856c08a42d3e
RIDHA OMRI <omriridha@hotmail.com>
2017-12-21 15:54:23 +0100

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoviesMVCTutorial.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
